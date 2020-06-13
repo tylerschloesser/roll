4a4435bf0401b3b64cf278bfca3ab74700883197
Tyler Schloesser <tschloes@amazon.com>
2020-06-13 13:56:07 -0700

    touch

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,43 @@ const context = canvas.getContext('2d')
 canvas.height = window.innerHeight
 canvas.width = window.innerWidth
 
-context.fillStyle = "grey";
-context.fillRect(0, 0, canvas.width, canvas.height)
+const cx = canvas.width / 2
+const cy = canvas.height / 2
+const cr = 50
 
-console.log(context)
+interface State {
+  touch: boolean
+}
+
+let state: State = {
+  touch: false
+}
+
+function draw(): void {
+  context.fillStyle = "grey";
+  context.fillRect(0, 0, canvas.width, canvas.height)
+
+  context.beginPath()
+  context.fillStyle = state.touch ? 'red' : 'blue'
+  context.arc(cx, cy, cr, 0, 2 * Math.PI)
+  context.fill()
+  context.stroke()
+
+  requestAnimationFrame(draw)
+}
+
+draw()
+
+document.addEventListener('touchstart', (e) => {
+  state = {
+    ...state,
+    touch: true,
+  }
+})
+
+document.addEventListener('touchend', (e) => {
+  state = {
+    ...state,
+    touch: false,
+  }
+})
