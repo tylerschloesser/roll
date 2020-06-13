6f5acb2cbcb3562b7e3c42c57cae192a48789b56
Tyler Schloesser <tschloes@amazon.com>
2020-06-13 14:52:53 -0700

    dir

--- src/lib.ts
+++ src/lib.ts
@@ -4,11 +4,20 @@ export function distance(a: Vec2, b: Vec2): number {
   return Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2))
 }
 
-export function scale(vec2: Vec2, m: number): Vec2 {
+export function multiply(vec2: Vec2, m: number) {
   return {
-    x: m,
-    y: m,
+    x: vec2.x * m,
+    y: vec2.y * m,
   }
+}
+
+export function normalize(vec2: Vec2): Vec2 {
+  const dist = distance({ x: 0, y: 0 }, vec2)
+  return multiply(vec2, 1 / dist)
+}
+
+export function scale(vec2: Vec2, m: number): Vec2 {
+  return multiply(normalize(vec2), m)
 }
 
 export function subtract(a: Vec2, b: Vec2) {
